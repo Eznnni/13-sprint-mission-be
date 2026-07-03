acfe0cb57598dfa7136ed4b53763bf99dcaa22aa
ezzzni <unin5412@gmail.com>
2026-07-03 13:19:35 +0900

    feat: errorHandler 생성 및 전역 미들웨어로 등록

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import articleRouter from "./routes/article.js";
 import productRouter from "./routes/product.js";
 import commentRouter from "./routes/comment.js";
 import { PORT } from "./constants/common.js";
+import errorHandler from "./middlewares/errorHandler.js";
 
 dotenv.config();
 
@@ -18,6 +19,9 @@ app.use("/products", productRouter);
 app.use("/articles", articleRouter);
 app.use("/comments", commentRouter);
 
+app.use(errorHandler);
+
+const port = process.env.PORT ?? 3001;
 app.listen(PORT, () => {
   console.log(`✅ Server running on http://localhost:${PORT}`);
 });
