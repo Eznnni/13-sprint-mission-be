d2b05fddf4c47086cb1f2d0c37b2cfa09493907c
ezzzni <unin5412@gmail.com>
2026-08-08 19:15:11 +0900

    feat: errorHandler 생성 및 적용

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,15 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import errorHandler from "./middlewares/errorHandler";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(errorHandler);
 
 const port = process.env.PORT ?? 3000;
 app.listen(port, () => {
