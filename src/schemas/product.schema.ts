7fddaf29060ff618fd1a619c349d529987ac594e
ezzzni <unin5412@gmail.com>
2026-08-10 16:44:48 +0900

    feat: zod product 유효성 검사 스키마 생성

---
+++ src/schemas/product.schema.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+export const createProductSchema = z.object({
+  name: z
+    .string()
+    .min(1, "상품명은 1자 이상이어야 합니다")
+    .max(10, "상품명은 10자 이내여야 합니다"),
+  description: z
+    .string()
+    .min(10, "상품 소개는 10자 이상이어야 합니다")
+    .max(100, "상품 소개는 100자 이내여야 합니다"),
+  price: z.coerce
+    .number()
+    .int()
+    .nonnegative()
+    .min(0, "판매 가격은 0원 이상이어야 합니다"),
+  tags: z.array(z.string().max(5, "태그는 5자 이내여야 합니다")).optional(),
+});
+
+export const updateProductSchema = createProductSchema.partial();
