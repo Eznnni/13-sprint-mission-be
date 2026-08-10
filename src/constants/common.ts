3508d076e30b05ec128b7385f1497bb428f6b164
ezzzni <unin5412@gmail.com>
2026-08-10 16:44:09 +0900

    feat: product 정렬 record type 생성

---
+++ src/constants/common.ts
@@ -0,0 +1,11 @@
+import { Prisma } from "../generated/prisma";
+
+export const PRODUCT_ORDERBY: Record<
+  string,
+  Prisma.ProductOrderByWithRelationInput
+> = {
+  recent: { createdAt: "desc" },
+  oldest: { createdAt: "asc" },
+  title: { name: "asc" },
+  like: { likeCount: "desc" },
+};
