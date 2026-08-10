9a0cec277c4b885c73b5c44582f087f74516f37b
ezzzni <unin5412@gmail.com>
2026-08-10 09:33:51 +0900

    chore: 400 badrequest error 커스텀 클래스 추가

--- src/types/errors.ts
+++ src/types/errors.ts
@@ -44,3 +44,10 @@ export class ForbiddenError extends AppError {
     this.name = "ForbiddenError";
   }
 }
+
+export class BadRequestError extends AppError {
+  constructor(message: string, data?: any) {
+    super(message, 400, data);
+    this.name = "BadRequestError";
+  }
+}
