00c9fcd323c9a79a2dede5a4dc6a7cfaa342fd73
ezzzni <unin5412@gmail.com>
2026-08-11 18:42:25 +0900

    chore: cors 설정

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,12 @@ import articleRouter from "./routes/article";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3001",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(cookieParser());
 
