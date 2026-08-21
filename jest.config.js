c53c761eeadcb8f6a45c914fe9afced6174fd3ac
ezzzni <unin5412@gmail.com>
2026-08-21 15:46:31 +0900

    chore: jest 라이브러리 적용 및 설정

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+const { createDefaultPreset } = require("ts-jest");
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+module.exports = {
+  testEnvironment: "node",
+  transform: {
+    ...tsJestTransformCfg,
+  },
+  collectCoverageFrom: [
+    "src/**/*.{js,ts}",
+    "!src/**/*.test.{js,ts}",
+    "!src/**/*.d.ts",
+  ],
+};
